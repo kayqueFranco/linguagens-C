a59f48ea668a6421a54373f2939e4b340278f8d1
Kayque <kayque.franco2018@gmail.com>
2024-11-05 17:26:10 -0300

    Nov exercicio com char

--- src/texto2.c
+++ src/texto2.c
@@ -4,6 +4,10 @@ int main(){
     char nome[20];
 
     printf("Escreva o seu primeiro nome e tecle Enter\n");
+    //Foi necessario utilizar colchetes para que o comando scanf receba toda a cadeia de caractes.
+    //O elemento ^ (circunflexo ) foi usado para indicar onde começa o primeiro caracter.E comando \n,nestecaso,
+    //está sendo usado para ler o espaço aplicado.
+    //ao final é utilizado o caracter s(string).
     scanf("%[^\n]s" ,&nome);
     
     printf("Olá, %s. Seja bem vindo\n",nome);
