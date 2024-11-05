f23a50a266e3402affefc2b483441afca6e2ede5
Kayque <kayque.franco2018@gmail.com>
2024-11-05 16:34:02 -0300

    Atividade de array

--- src/array5.c
+++ src/array5.c
@@ -4,13 +4,17 @@ int main (){
     int n[]={13,8,23,95,47,14,10,45,165};
 
     int soma = 0 ;
+    int media ;
 
     for (int i = 0; i< 9; i++){
         soma += n[i];
+        media = soma/9;
+        
 
     }
 
-    printf("A soma dos valores é %d\n",soma);
+    printf("A soma dos valores é %d e a media é %d\n",soma ,media);
+    
 
 
     return 0;
