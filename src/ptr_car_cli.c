50c9b577bd377c64627aabdb2473551dcef50799
Kayque <kayque.franco2018@gmail.com>
2024-11-14 14:48:58 -0300

    realização da listas

--- src/ptr_car_cli.c
+++ src/ptr_car_cli.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 
-void cadastro(char *nome, char *email, char *idade){
+void cadastro(char *nome, char *email, int *idade){
     FILE *arquivo;
     arquivo= fopen("files/ptr_cli.txt","a");
-    fprintf(arquivo,nome);
-    fprintf(arquivo,email);
-    fprintf(arquivo,idade);
+    fprintf(arquivo,"Nome:%s\n",nome);
+    fprintf(arquivo,"Email:%s\n",email);
+    fprintf(arquivo,"Idade:%d\n",*idade);
+    fprintf(arquivo,"-------------------------------\n"); 
     fclose(arquivo);
 }
 
 int main(){
     char nome[30];
     char email[50];
-    char idade [2] ;
+    int idade  ;
 
     printf("Digite o seu nome e tecle Enter\n");
     scanf("%[^\n]s",nome);
     printf("Digite o seu email e tecle Enter\n");
     scanf("%s",email);
     printf("Digite sua idade e tecle Enter\n");
-    scanf("%s",idade);
-    cadastro(nome,email,idade);
+    scanf("%d",&idade);
+    cadastro(nome,email,&idade);
     printf("Cadstro?\n");
     return 0;
 }
